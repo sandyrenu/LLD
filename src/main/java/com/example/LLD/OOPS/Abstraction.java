ea79b0c0ca498075147126bdf32d4c5bf46c9b71
Sandeep Renu <Sandeep.Patchala@tanla.com>
2024-10-20 17:52:41 +0530

    Added Abstraction examples

--- src/main/java/com/example/LLD/OOPS/Abstraction.java
+++ src/main/java/com/example/LLD/OOPS/Abstraction.java
@@ -1,2 +1,32 @@
-package com.example.LLD.OOPS;public class Abstraction {
+package com.example.LLD.OOPS;
+
+ abstract class Vehicle {
+    // Abstract method (no implementation)
+    abstract void accelerate();
+
+    // Concrete method (implementation provided)
+    void start() {
+        System.out.println("Vehicle is starting...");
+    }
 }
+
+class Car extends Vehicle {
+    @Override
+    void accelerate() {
+        System.out.println("Car is accelerating...");
+    }
+}
+
+
+// This class ideally main function
+public class Abstraction {
+    public static void abstraction(String[] args) {
+        // Creating an object of Car class
+        Vehicle myCar = new Car();
+        myCar.start();         // Abstracted behavior
+        myCar.accelerate();     // Implementation hidden, abstracted behavior
+    }
+}
+
+
+// Abstract classes doesnt have multiple inheritance due to diamond problem
