d5f26b60f1ac6149a822a6839d91e52ef13f4913
Sandeep Renu <Sandeep.Patchala@tanla.com>
2024-11-12 22:52:56 +0530

    Two Sum approach

---
+++ src/main/java/DSA/BLIND75/TwoSum.java
@@ -0,0 +1,22 @@
+package DSA.BLIND75;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+        // problem statement is need to add the two integers
+        int a[] = new int[]{1,2,3,4,5};
+
+        // need to add the two integers and the target will be the sum variable
+
+        // O(n^2)
+
+        //brute force
+        // add two loops and implement
+
+        // O (nlog n)
+        // sort the array and do two pointer approach
+
+        // o(n)
+        // using HashMap
+    }
+}
