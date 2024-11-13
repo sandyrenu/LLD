ff56537a299af99615582744e23b4e47abac4037
Sandeep Renu <Sandeep.Patchala@tanla.com>
2024-11-13 08:06:26 +0530

    Two Sum Brute Force

--- src/main/java/DSA/BLIND75/TwoSum.java
+++ src/main/java/DSA/BLIND75/TwoSum.java
@@ -5,6 +5,7 @@ public class TwoSum {
     public static void main(String[] args) {
         // problem statement is need to add the two integers
         int a[] = new int[]{1,2,3,4,5};
+        int target = 5;
 
         // need to add the two integers and the target will be the sum variable
 
@@ -12,6 +13,17 @@ public class TwoSum {
 
         //brute force
         // add two loops and implement
+
+        for(int i = 0 ; i < a.length - 1; i++){
+            for(int j = i + 1 ; j < a.length - 2 ; j++){
+                if(a[i] + a[j] == target){
+                    System.out.println(i);
+                    System.out.println(j);
+                    break;
+                }
+            }
+
+        }
 
         // O (nlog n)
         // sort the array and do two pointer approach
